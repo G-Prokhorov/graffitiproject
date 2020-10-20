be35be5dadf92c77ac4581f827e4ace790191a2d
Prokhorov <grisha.prokhorov03@gmail.com>
2020-10-20 11:02:45 +0300

    Preview menu

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const path = require('path');
-const multer = require('multer')
-const cors = require("cors")
+const multer = require('multer');
+const cors = require("cors");
 const app = express();
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const cookieParser = require('cookie-parser');
-require('dotenv').config()
+require('dotenv').config();
 
 const DataPainters = require("./model/painterdb");
-const User = require("./model/userdb")
+const User = require("./model/userdb");
 
 const secret = process.env.SECRET;
-const fileSize = 500000000000000000000000000000000000000000; 
+const fileSize = 500000000000000000000000000000000000000000;
 
 app.use(express.static(path.join(__dirname, "build")));
 app.use(bodyParser.json());
@@ -26,16 +26,14 @@ app.use(cors({
 app.use(cookieParser());
 
 const saltRounds = 10;
-      
+
 app.post("/test", withAuth, (req, res) => {
       res.send("lmao");
-})
-
-app.get("/ping", (req, res)=>{
-      return res.send("pong");
 });
 
-app.get("/", (req, res)=>{
+app.get("/ping", (req, res) => res.send("pong"));
+
+app.get("/", (req, res) => {
       res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
@@ -43,54 +41,54 @@ app.get("/", (req, res)=>{
 
 
 app.get("/api/db", async (req, res) => {
-      
+
       try {
-           let item = await  DataPainters.find({});
-           res.json(item)
+            const item = await DataPainters.find({});
+            res.json(item);
       } catch (err) {
             console.error("Error while read DB, " + err);
             res.sendStatus(500)
       }
-          
-   
+
+
 });
 
 app.get("/api/painter/:id", async (req, res) => {
       try {
-            let item = await DataPainters.findOne({nick: req.params.id});
+            const item = await DataPainters.findOne({ nick: req.params.id });
             res.json(item);
-      } catch (err){
+      } catch (err) {
             console.error("Error while find item, " + err);
-            res.sendStatus(500)
+            res.sendStatus(500);
       }
 });
 
 
 
 
-let storage = multer.memoryStorage();
-let upload = multer({ storage: storage,  limits: { fileSize: fileSize} } );
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage, limits: { fileSize: fileSize } });
 
 
 app.post('/upload', upload.any(), withAuth, async (req, res) => {
       let nick = req.body.nick;
       nick = nick.toUpperCase()
-      
+
       const data = {
             tag: "",
             works: []
       }
       console.log("start")
       await Promise.all(req.files.map(async (file) => {
-           
+
             if (!file) {
                   res.json("You may be not upload some file")
             } else if (file.fieldname === "tag") {
-                  let filename = Date.now() + '-' +file.originalname;
+                  let filename = Date.now() + '-' + file.originalname;
                   let fileContent = file.buffer;
                   data.tag = await uploadFile(fileContent, filename);
             } else if (file.fieldname === "file") {
-                  let filename = Date.now() + '-' +file.originalname;
+                  let filename = Date.now() + '-' + file.originalname;
                   let fileContent = file.buffer;
                   data.works.push(await uploadFile(fileContent, filename));
             } else {
@@ -98,101 +96,106 @@ app.post('/upload', upload.any(), withAuth, async (req, res) => {
             }
       }));
 
-      try { 
-            let item = await DataPainters.findOne({nick: nick});
+      try {
+            let item = await DataPainters.findOne({ nick: nick });
             if (item && !data.tag) {
-                  
-                  if (item.tag !== null)  {
-                  data.tag=item.tag
+
+                  if (item.tag !== null) {
+                        data.tag = item.tag
                   }
             } else if (item && data.tag) {
-                if (item.tag !== null)  {
-                      deleteFile(item.tag.key)
+                  if (item.tag !== null) {
+                        deleteFile(item.tag.key)
                   }
             }
-        } catch(err) {
-              console.error(err);
-        }
+      } catch (err) {
+            console.error(err);
+      }
 
 
       try {
-            await  DataPainters.findOneAndUpdate({nick: nick}, {
+            await DataPainters.findOneAndUpdate({ nick: nick }, {
                   nick: nick,
-                  $set: {tag: data.tag},
-                  $push: {works: data.works}
+                  $set: { tag: data.tag },
+                  $push: { works: data.works }
             }, {
-            upsert: true,
+                  upsert: true,
             });
-      } catch(err) {
+      } catch (err) {
             console.error("Error while update file, " + err)
       }
 
-      });
+});
 
-      
-      
 
-      app.post("/deleteWork", withAuth, async (req, res) => {
-            let key = req.body.key;
-            let nick = req.body.nick;
 
-            try {
-                  await  DataPainters.findOneAndUpdate({nick: nick}, {
-                        $pull: {works: {key: key}}
-                  });
-                  deleteFile(key);
-            } catch(err) {
-                  console.error("Error while update file, " + err)
+
+app.post("/deleteWork", withAuth, async (req, res) => {
+      let key = req.body.key;
+      let nick = req.body.nick;
+
+      try {
+            await DataPainters.findOneAndUpdate({ nick: nick }, {
+                  $pull: { works: { key: key } }
+            });
+            deleteFile(key);
+      } catch (err) {
+            console.error("Error while update file, " + err)
+      }
+      res.redirect("http://localhost:3000/addmenu")
+})
+
+app.post("/DeletePainter", withAuth, async (req, res) => {
+      console.log(req.body.nick)
+      let nick = req.body.nick; // MUST BE req.body.nick
+      try {
+            let item = await DataPainters.findOne({ nick: nick });
+            console.log(item.tag.key);
+            if (item.tag.key) {
+                  await deleteFile(item.tag.key);
             }
-            res.redirect("http://localhost:3000/addmenu")
-      })
+            item.works.map(async (item) => {
+                  await deleteFile(item.key);
+            });
+            console.log("all ok")
+      } catch (err) {
+            console.error("Error while read db, " + err)
+      }
+      console.log("here")
+      try {
+            await DataPainters.findOneAndDelete({ nick: nick })
+      } catch (err) {
+            console.error("Error while delete painter, " + err)
+      }
 
-      app.post("/DeletePainter", withAuth, async (req, res) => {
-            console.log(req.body.nick)
-           let nick = req.body.nick; // MUST BE req.body.nick
-            try {
-                  let item = await DataPainters.findOne({nick: nick});
-                  console.log(item.tag.key);
-                  if (item.tag.key) {
-                        await deleteFile(item.tag.key);
+      res.redirect("http://localhost:3000/")
+})
+
+function withAuth(req, res, next) {
+      console.log("here")
+      const token = req.body.token;
+      var test = "LMAO";
+
+      if (!token) {
+            res.status(401).send('Unauthorized: No token provided');
+      } else {
+            jwt.verify(token, secret, function (err, decoded) {
+
+                  if (err) {
+                        res.status(401).send('Unauthorized: Invalid token');
+                  } else {
+                        console.log(decoded)
+                        req.email = decoded.email;
+                        next();
                   }
-                  item.works.map( async (item) => {
-                        await deleteFile(item.key);
-                  });
-                  console.log("all ok")
-            } catch (err) {
-                  console.error("Error while read db, " + err)
-            }
-            console.log("here")
-            try {
-                  await  DataPainters.findOneAndDelete({nick: nick})
-            } catch (err) {
-                  console.error("Error while delete painter, " + err)
-            }
-
-            res.redirect("http://localhost:3000/")
-      }) 
-
-      function withAuth (req, res, next) {
-            const token = req.body.token;              
-            if (!token) {
-              res.status(401).send('Unauthorized: No token provided');
-            } else {
-              jwt.verify(token, secret, function(err, decoded) {
-                if (err) {
-                  res.status(401).send('Unauthorized: Invalid token');
-                } else {
-                  req.email = decoded.email;
-                  next();
-                }
-              });
-            }
-          }
+            });
+      }
+}
 
 //////////////////////////Cloud servise//////////////////////////////////////////////////
 var AWS = require('aws-sdk');
 
-var credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
+var credentials = new AWS.SharedIniFileCredentials({ profile: 'default' });
 AWS.config.credentials = credentials;
 
 const s3 = new AWS.S3();
@@ -207,13 +210,14 @@ async function uploadFile(fileContent, fileName) {
             ACL: "public-read"
       };
 
-     let result = await new Promise((resolve)=>{s3.upload(params, (err, data)=>{
+      let result = await new Promise((resolve) => {
+            s3.upload(params, (err, data) => {
                   if (err) {
                         console.log(err);
                   }
-                    resolve(data.Location);
-                  })
-            });
+                  resolve(data.Location);
+            })
+      });
       return {
             key: fileName,
             link: result
@@ -227,16 +231,16 @@ function deleteFile(key) {
             Bucket: "graffitiproject",
             Key: key
       };
-      s3.deleteObject(params, (err, data)=>{
+      s3.deleteObject(params, (err, data) => {
             if (err) {
                   console.error(err)
             }
-        });
-   
-    
+      });
+
+
 
 }
- 
+
 
 
 //////////////////////////Cloud servise//////////////////////////////////////////////////
@@ -245,37 +249,41 @@ function deleteFile(key) {
 
 app.post('/api/register', async (req, res) => {
       console.log(req.body)
-      const {email, password} = req.body.data;
+      const { email, password } = req.body.data;
       try {
-           let salt = await bcrypt.genSalt(saltRounds);
-           let hash = await bcrypt.hash(password, salt);
-           await User.findOneAndUpdate({email: email}, 
-            {$setOnInsert: 
-                  { email: email,
-                  password: hash } 
-            },
-            { upsert: true });
+            let salt = await bcrypt.genSalt(saltRounds);
+            let hash = await bcrypt.hash(password, salt);
+            await User.findOneAndUpdate({ email: email },
+                  {
+                        $setOnInsert:
+                        {
+                              email: email,
+                              password: hash
+                        }
+                  },
+                  { upsert: true });
             res.sendStatus(200)
       } catch (err) {
             res.sendStatus(500)
             console.log("Error while register new user, " + err)
       }
-     
-      });
+
+});
 
 app.post("/api/login", async (req, res) => {
       let body = req.body.data;
-      const {email, password} = body;
+      const { email, password } = body;
       try {
-            let item = await User.findOne({email: email});
+            let item = await User.findOne({ email: email });
             let result = await bcrypt.compare(password, item.password);
             if (result === false) {
                   res.sendStatus(401).send("Incorect passwor or email");
-            } else if (result === true) { 
-                  let token = await jwt.sign({email: email}, secret, {
+            } else if (result === true) {
+                  let token = await jwt.sign({ email: email }, secret, {
                         expiresIn: '1h'
-                      });
-                  res.json({token: token});   
+                  });
+                  res.json({ token: token });
+
             }
       } catch (err) {
             console.log("Error while login new user, " + err)
@@ -283,23 +291,25 @@ app.post("/api/login", async (req, res) => {
       }
 });
 
-app.post('/api/checkToken', function(req, res) {
+app.post('/api/checkToken', async function (req, res) {
       const token = req.body.token;
       if (!token) {
             res.status(401).send('Unauthorized: No token provided');
-          } else {
-            jwt.verify(token, secret, function(err, decoded) {
-              if (err) {
-                res.status(401).send('Unauthorized: Invalid token');
-              } else {
-                res.status(200).send("All ok")
-              }
+      } else {
+            let aa = await jwt.verify(token, secret, function (err, decoded) {
+                  console.log(decoded)
+                  if (err) {
+                        res.status(401).send('Unauthorized: Invalid token');
+                  } else {
+                        res.status(200).send("All ok")
+                  }
             });
-          }
-        }
-    )
+            console.log(aa)
+      }
+}
+)
 ///////////////////////////////Login & Register///////////////////////////////////////////////
 
-app.listen(8080, ()=>{
+app.listen(8080, () => {
       console.log("Server started on port 8080")
 });
--- src/components/AddForm.jsx
+++ src/components/AddForm.jsx
@@ -21,42 +21,59 @@ function AddForm() {
       }
 
       Start();
-      
-      const   [state, setState] = useState({
-            selectedFile:[String],
-            tag: "", 
-            nick:""
+
+
+      const [state, setState] = useState({
+            selectedFile: [String],
+            tag: "",
+            nick: ""
       });
       let [array, setArray] = useState([])
-      const [preview, setPreview] = useState(null);
+      const [preview, setPreview] = useState({
+            tag: null,
+            works: []
+      });
       let [existingWorks, setExistingWorks] = useState({
             oldTag: "",
             array: [],
             show: false
       });
 
+      console.log(preview.works)
+
       useEffect(async () => {
             try {
-            let data = await Axios.get("http://localhost:8080/api/db")
-            console.log(data);
-            data.data.map((element) => {
-                  setArray((prev) => {
-                        return [...prev, element.nick]
-                  })
-            });
-      } catch(err) {
-            console.error("Error while get info about users, " + err)
-      }
+                  let data = await Axios.get("http://localhost:8080/api/db")
+                  console.log(data);
+                  data.data.forEach((element) => {
+                        setArray((prev) => [...prev, element.nick])
+                  });
+            } catch (err) {
+                  console.error("Error while get info about users, " + err)
+            }
       }, []);
 
-      function   onChangeHandler(event){
-            let files = event.target.files;
+      async function onChangeHandler(event) {
+            let files = [...event.target.files];
+
             setState((prev) => {
-                  return {nick: prev.nick,
-                  selectedFile: files,
-                  tag: prev.tag
-            }});
-        }
+                  return {
+                        nick: prev.nick,
+                        selectedFile: files,
+                        tag: prev.tag
+                  }
+            });
+
+            files.forEach((element) => {
+                  setPreview((prev) => {
+                        return {
+                              ...prev,
+                              works: [...prev.works, URL.createObjectURL(element)]
+                        }
+                  })
+            })
+
+      }
 
       async function onChangeNick(event) {
             let text = event.target.value;
@@ -64,50 +81,62 @@ function AddForm() {
             if (array.includes(text)) {
                   UppdateWorks(text);
             } else {
-                  setExistingWorks({oldTag: "",
-                  array: [],
-                  show: false})
+                  setExistingWorks({
+                        oldTag: "",
+                        array: [],
+                        show: false
+                  })
             }
 
 
-            setState((prev)=>{
-                  return {nick: text,
-                  selectedFile: prev.selectedFile,
-                  tag: prev.tag
-            }});
+            setState((prev) => {
+                  return {
+                        nick: text,
+                        selectedFile: prev.selectedFile,
+                        tag: prev.tag
+                  }
+            });
       }
 
       function onChangeTag(event) {
             let file = event.target.files[0];
             setState((prev) => {
-                  return {nick: prev.nick,
-                  selectedFile: prev.selectedFile,
-                  tag: file
-            }});
-            setPreview(URL.createObjectURL(file));
+                  return {
+                        nick: prev.nick,
+                        selectedFile: prev.selectedFile,
+                        tag: file
+                  }
+            });
+            setPreview((prev) => {
+                  return {
+                        ...prev,
+                        tag: URL.createObjectURL(file)
+                  }
+            });
       }
 
-      function  onClickHandler() {
+      function onClickHandler() {
             let data = new FormData();
-            Array.prototype.forEach.call(state.selectedFile, (element)=>{
+            Array.prototype.forEach.call(state.selectedFile, (element) => {
                   data.append("file", element);
             });
             data.append('nick', state.nick);
             data.append('tag', state.tag);
             data.append("token", JSON.parse(localStorage.getItem('user')))
             Axios.post("http://localhost:8080/upload", data
-            ).then(res => { 
+            ).then(res => {
                   console.log(res.status);
                   UppdateWorks(state.nick);
-               })
-            }
+            })
+      }
 
-            async function DeletePainter() {
-                  if   ( window.confirm(`Are you sure?`)) {
-                    try  {
-                        let dataDelPainter = {'nick': state.nick,
-                        "token": JSON.parse(localStorage.getItem('user'))
-                  };
+      async function DeletePainter() {
+            if (window.confirm(`Are you sure?`)) {
+                  try {
+                        let dataDelPainter = {
+                              'nick': state.nick,
+                              "token": JSON.parse(localStorage.getItem('user'))
+                        };
                         let res = await Axios.post("http://localhost:8080/DeletePainter", dataDelPainter);
                         console.log(res);
                   } catch (err) {
@@ -115,98 +144,109 @@ function AddForm() {
                   }
                   UppdateWorks();
             }
-            }
+      }
 
-            async function DeleteWorks(event) {
-                  console.log("here")
-                  try {
-                        let dataDelWork = {
-                              'nick': state.nick,
-                              "key": event.target.value,
-                              "token": JSON.parse(localStorage.getItem('user'))
-                        }
-                        await Axios.post("http://localhost:8080/deleteWork", dataDelWork);
-                  } catch (err) {
-                        console.error(err)
+      async function DeleteWorks(event) {
+            console.log("here")
+            try {
+                  let dataDelWork = {
+                        'nick': state.nick,
+                        "key": event.target.value,
+                        "token": JSON.parse(localStorage.getItem('user'))
                   }
-                  UppdateWorks(state.nick);
+                  await Axios.post("http://localhost:8080/deleteWork", dataDelWork);
+            } catch (err) {
+                  console.error(err)
             }
+            UppdateWorks(state.nick);
+      }
+      console.log(preview)
 
       async function UppdateWorks(text) {
             console.log(text);
-            setExistingWorks({oldTag: "",
+            setExistingWorks({
+                  oldTag: "",
                   array: [],
-                  show: false});
-           if (text) {
-                 try {
-                  let res = await Axios.get("http://localhost:8080/api/painter/"+ text);
-                  console.log(res)
-                  let tag = res.data.tag.link;
-                  setExistingWorks((prev) => {
-                        return {
-                              ...prev, 
-                              oldTag: tag,
-                              show: true
-                        }
-                  })
-                  res.data.works.map((element) => {
+                  show: false
+            });
+            if (text) {
+                  try {
+                        let res = await Axios.get("http://localhost:8080/api/painter/" + text);
+                        console.log(res)
+                        let tag = res.data.tag.link;
                         setExistingWorks((prev) => {
-                              return {...prev,
-                                 array: [...prev.array, element]
+                              return {
+                                    ...prev,
+                                    oldTag: tag,
+                                    show: true
                               }
                         })
-                  })
-            }  catch(err) {
-                  console.log("Error receiving painter's work, " + err)
-            } }
+                        res.data.works.map((element) => {
+                              setExistingWorks((prev) => {
+                                    return {
+                                          ...prev,
+                                          array: [...prev.array, element]
+                                    }
+                              })
+                        })
+                  } catch (err) {
+                        console.log("Error receiving painter's work, " + err)
+                  }
+            }
       }
 
       if (!Token) {
             return <LoginPage />
       } else if (Token) {
             return (<div>
-                  <Title title="Add menu"/>
+                  <Title title="Add menu" />
                   <div className="container">
                         <div className="AddForm">
                               <label for="text">Nickname</label>
-                              <input type="text" className="InputText" onChange={onChangeNick} value={state.nick}/>
+                              <input type="text" className="InputText" onChange={onChangeNick} value={state.nick} />
                               <label for="tag">Tag</label>
-                              <input onChange={onChangeTag} type="file" id="tag" name="tag" calssName="InputFile" accept=".jpg, .jpeg, .png"/>
+                              <input onChange={onChangeTag} type="file" id="tag" name="tag" calssName="InputFile" accept=".jpg, .jpeg, .png" />
                               <label for="file">Works</label>
-                              <input onChange={onChangeHandler} type="file" id="file" name="file" calssName="InputFile" accept=".jpg, .jpeg, .png" multiple/>
-                              
+                              <input onChange={onChangeHandler} type="file" id="file" name="file" calssName="InputFile" accept=".jpg, .jpeg, .png" multiple />
+
                               <button className="styleBth" type="submit" onClick={onClickHandler}>Add</button>
                               <ErrValidator visibility={existingWorks.show} err="This painter alredy exist, but you can edit his profile if you like" />
-
                         </div>
                   </div>
-                  <Title title="Preview"/>
+                  <Title title="Preview" />
                   <div className="preview">
                         <div className="container center bigCard">
-                              <Card  name={state.nick.toUpperCase()}/>
-                              <Tag oldTag={existingWorks.oldTag} tag={preview} />
+                              <Card name={state.nick.toUpperCase()} />
+                              <Tag oldTag={existingWorks.oldTag} tag={preview.tag} />
                         </div>
                         <div className="container center">
-                        {existingWorks.array.map((element)=> {
-                              return <div className="WorkBlock">
-                              <img className="painterWorks" src={element.link} />
-                              <div className="DeleteItem">
-                                    <button onClick={DeleteWorks} value={element.key} type="submit" name="key" className="DeleteBth">del</button>
-                              </div>
-            </div>
-                        })}
+                              {existingWorks.array.map((element) => {
+                                    return <div className="WorkBlock">
+                                          <img className="painterWorks" src={element.link} />
+                                          <div className="DeleteItem">
+                                                <button onClick={DeleteWorks} value={element.key} type="submit" name="key" className="DeleteBth">del</button>
+                                          </div>
+                                    </div>
+                              })}
+
+                              {preview.works.map((element) => {
+                                    return <div className="WorkBlock">
+                                          <img className="painterWorks" src={element} />
+                                    </div>
+                              })}
+
 
                         </div>
                   </div>
-            <div className="container center">
-                  <button onClick={DeletePainter} className="styleBth center" style={existingWorks.show ? {visibility: "visible", display: "block"} : {visibility: "hidden", display: "none"}}>Delete painter</button>
-            </div>
-                  <Footer />   
-            </div>)       
+                  <div className="container center">
+                        <button onClick={DeletePainter} className="styleBth center" style={existingWorks.show ? { visibility: "visible", display: "block" } : { visibility: "hidden", display: "none" }}>Delete painter</button>
+                  </div>
+                  <Footer />
+            </div>)
       }
 
 
-      
+
 }
 
 export default AddForm;
