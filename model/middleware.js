07c452ec6dfdc6c8e0f85ba7bdc9175a014145ab
Prokhorov <grisha.prokhorov03@gmail.com>
2020-11-06 23:51:54 +0200

    jwt w/ check email

--- model/middleware.js
+++ model/middleware.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const User = require("./userdb");
 
 const secret = process.env.SECRET;
 const refreshSecret = process.env.REFRESHSECRET;
@@ -10,35 +11,53 @@ function withAuth(req, res, next) {
       if (!token) {
             res.status(401).send('Unauthorized: No token provided');
       } else {
-            jwt.verify(token, secret, function (err, decoded) {
-                  console.log(decoded);
+            jwt.verify(token, secret, async function (err, decoded) {
                   if (err) {
                         // res.status(401).send('Unauthorized: Invalid token');
-                        jwt.verify(refreshToken, refreshSecret, function (err, decoded) {
-                              console.log(decoded);
+
+
+                        jwt.verify(refreshToken, refreshSecret, async function (err, decoded) {
                               if (err) {
                                     res.status(401).send('Unauthorized: Invalid token');
-
                               } else {
-                                    const token = jwt.sign({ email: decoded.email }, secret, {
-                                          expiresIn: '1h'
-                                    });
-                                    const refreshToken = jwt.sign({ email: decoded.email }, refreshSecret, {
-                                          expiresIn: '7d'
-                                    });
-                                    res.json({
-                                          token: token,
-                                          refreshToken: refreshToken
-                                    }).status(200).send("All ok");
-                                    next();
-
+                                    const check = await searchInBd(decoded.email);
+                                    if (check) {
+                                          const token = jwt.sign({ email: decoded.email }, secret, {
+                                                expiresIn: '1h'
+                                          });
+                                          const refreshToken = jwt.sign({ email: decoded.email }, refreshSecret, {
+                                                expiresIn: '7d'
+                                          });
+                                          res.json({
+                                                token: token,
+                                                refreshToken: refreshToken
+                                          }).status(200);
+                                          next();
+                                    }
                               }
                         });
+
                   } else {
-                        res.status(200).send("All ok");
-                        next();
+                        const check = await searchInBd(decoded.email);
+                        if (check) {
+                              res.status(200).send("All ok");
+                              next();
+                        }
                   }
             });
+      }
+}
+
+async function searchInBd(email) {
+      try {
+            const user = await User.findOne({ email: email });
+            if (user.email) {
+                  return true;
+            }
+      }
+      catch (err) {
+            res.status(401).send('Unauthorized: Invalid token');
+            return false;
       }
 }
 
