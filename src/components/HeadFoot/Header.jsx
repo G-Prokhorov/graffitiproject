3aa353a231f88cad4c1a8ef537ac3cc5f8ec71cc
Gregory Prokhorov <grisha.prokhorov03@gmail.com>
2021-03-11 08:14:05 +0200

    new Header

---
+++ src/components/HeadFoot/Header.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import AuthSetvice from "../AuthSetvice";
+
+import "./style.css";
+
+
+function Header(props) {
+      let [Token, setToken] = useState(false)
+
+      useEffect(() => {
+            let token = JSON.parse(localStorage.getItem('user'));
+            if (token) {
+                  setToken(true);
+            } else {
+                  setToken(false);
+            }
+      }, []);
+
+
+      function logout() {
+            AuthSetvice.logout();
+            window.location.replace("/home");
+      }
+
+
+      return (<header className="header">
+            <h1>Graffiti Kharkiv</h1>
+            <Link to="/"><p>Home</p></Link>
+            <div className="header-right">
+                  {Token ?
+                        <button onClick={logout}>Log out</button>
+                        : <Link to="/login">
+                              <button>Log in</button>
+                        </Link>
+                  }
+            </div>
+      </header>);
+}
+
+
+
+
+export default Header;
+
