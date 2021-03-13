5a60ef3eb00bccfcf43e9bfe2c98f6437b5c42a5
Gregory Prokhorov <grisha.prokhorov03@gmail.com>
2021-03-13 08:46:11 +0200

    new AboutMe

---
+++ src/components/AboutMe/Aboutme.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import "./style.css";
+
+import Title from "../Title/Title";
+import Card from "../Cards/Card";
+
+function Aboutme() {
+      return (
+            <div id={"aboutMe"}>
+                  <Title title="About me" />
+                  <div className="container AboutMe">
+                        <div class="right">
+                              <p style={{ fontSize: '40px' }}>Gregory Prokhorov</p>
+                              <ul>
+                                    <li>18 y.o.</li>
+                                    <li>Studying in KhPI</li>
+                                    <li>English level: intermediate</li>
+                                    <li>Love programing &#60;3</li>
+                              </ul>
+                              <div className="myCard">
+                                    <Card name="Prkhrv" />
+                              </div>
+                        </div>
+                        <div className="AboutMeImg"></div>
+                  </div>
+            </div >)
+}
+
+export default Aboutme;
