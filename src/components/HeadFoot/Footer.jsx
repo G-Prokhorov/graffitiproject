8e54fc702ad1b7577a2536dae874be89139f7bb6
Gregory Prokhorov <grisha.prokhorov03@gmail.com>
2021-03-13 08:03:24 +0200

    Footer icon

--- src/components/HeadFoot/Footer.jsx
+++ src/components/HeadFoot/Footer.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import instagram from "../img/icon/instagram.svg"
+import twitter from "../img/icon/twitter.svg"
+import linkedin from "../img/icon/linkedin.svg"
+import telegram from "../img/icon/telegram.svg"
+
 import "./style.css"
 
 function Footer(props) {
@@ -14,19 +19,19 @@ function Footer(props) {
                               </tr>
                               <tr>
                                     <td><Link to="/">Home</Link></td>
-                                    <td><a href="https://www.instagram.com/g_prkhrv/">Instagram</a></td>
+                                    <td><a href="https://www.instagram.com/g_prkhrv/" target="_blank"><img className="icons" src={instagram} alt="" />Instagram</a></td>
                               </tr>
                               <tr>
                                     <td><Link to="/#aboutMe">About me</Link></td>
-                                    <td><a href="https://twitter.com/GPrkhrv?s=09">Twitter</a></td>
+                                    <td><a href="https://twitter.com/GPrkhrv?s=09" target="_blank"><img className="icons" src={twitter} alt="" />Twitter</a></td>
                               </tr>
                               <tr>
                                     <td><Link to="/#painers">Painters</Link></td>
-                                    <td><a href="https://www.linkedin.com/in/gregory-prokhorov-595302201/">Linkedin</a></td>
+                                    <td><a href="https://www.linkedin.com/in/gregory-prokhorov-595302201/" target="_blank"><img className="icons" src={linkedin} alt="" />Linkedin</a></td>
                               </tr>
                               <tr>
                                     <td><Link to="/login">Login</Link></td>
-                                    <td><a href="https://t.me/grisha_prokhorov">Telegram</a></td>
+                                    <td><a href="https://t.me/grisha_prokhorov" target="_blank"><img className="icons" src={telegram} alt="" />Telegram</a></td>
                               </tr>
                         </table>
 
