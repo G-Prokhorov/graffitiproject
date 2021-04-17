a380275762c94a1962bf2d65339eac40e8af709c
Gregory Prokhorov <grisha.prokhorov03@gmail.com>
2021-04-17 21:02:15 +0300

    route by hash

--- src/components/HeadFoot/Footer.jsx
+++ src/components/HeadFoot/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { HashLink as Link } from 'react-router-hash-link';
 
 import instagram from "../img/icon/instagram.svg"
 import twitter from "../img/icon/twitter.svg"
